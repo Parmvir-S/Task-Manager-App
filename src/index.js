980da9847aa11984f2e18b4b89f46dd6c85d6d28
Parmvir-S <parmvir.shergill@gmail.com>
2022-02-10 23:31:12 -0800

    worked on profile pic upload

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ const taskRouter = require("./routers/tasks");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const multer = require("multer");
+const upload = multer({
+    dest: "images"
+})
+
+app.post("/upload", upload.single("upload"), (req, res) => {
+    res.send();
+})
+
+
 app.use(express.json()); //automatically parse incoming json to an object 
 app.use(userRouter);
 app.use(taskRouter);
@@ -14,18 +24,3 @@ app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
 
-
-// const Task = require("./models/task");
-// const User = require("./models/user");
-
-// const main = async () => {
-//     // const task = await Task.findById("61e4fe33ab6762c4b197d7fb");
-//     // await task.populate("owner");
-//     // console.log(task.owner)
-    
-//     const user = await User.findById("61e4fc6e0df23099a905ccea");
-//     await user.populate("tasks");
-//     console.log(user.tasks)
-// }
-
-// main();
